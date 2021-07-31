0b5c467844351351ebfaa6cc04769e482cad8820
James Yoo <jyoo3607@gmail.com>
2021-07-31 09:20:34 -0700

    working on footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -3,10 +3,13 @@ import './Footer.css'
 
 const Footer = () => {
     return (
-       <div className="footer">
-           This is the footer element
-       </div>
+        <div className="row">
+                <h3>
+                    This website was created using React.Js
+                </h3>
+        </div>
+   
     )
 }
 
-export default Footer
+export default Footer;
