29d33445599084ce476797eef3b91f11e0f22df9
James Yoo <jyoo3607@gmail.com>
2021-07-30 19:37:13 -0700

    adding on the navbar

---
+++ src/components/Navbar.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import './Navbar.css'
+const Navbar = () => {
+    return (
+        <div className="navbar">
+            <div className="navbar-logo">
+                LOGO
+            </div>
+            <div className="navbar-menu">
+                <ul>
+                    <li><a href="">.about()</a></li>
+                    <li><a href="">.work()</a></li>
+                    <li><a href="">.contact()</a></li>
+                    <li><a href="">.about()</a></li>
+                </ul>
+            </div>
+        </div>
+    )
+}
+
+export default Navbar;
